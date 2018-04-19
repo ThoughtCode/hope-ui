549f19b95b281418d911e2bb608d46471325ed09
Rai Romero <rainieromadrid@gmail.com>
2018-04-19 16:52:58 -0500

    Fixing lost token in register
    Signed-off-by: Rai Romero <rainieromadrid@gmail.com>

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
     return {
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null || localStorage.getItem('token')
     };
 };
 

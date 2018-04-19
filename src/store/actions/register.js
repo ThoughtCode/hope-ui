7d8222323c35c18be74af259604bcdd6d19dec15
Rai Romero <rainieromadrid@gmail.com>
2018-04-19 16:49:05 -0500

    Creanting CRUD for Properties WIP
    Signed-off-by: Rai Romero <rainieromadrid@gmail.com>

--- src/store/actions/register.js
+++ src/store/actions/register.js
@@ -1,6 +1,10 @@
 import { push } from 'react-router-redux';
 import * as actionTypes from './actionTypes';
+<<<<<<< b6ae16fe45b752907b55e74f60d558846682f2fb
 import axios from '../../axios-prod';
+=======
+import axios from '../../axios-dev';
+>>>>>>> Creanting CRUD for Properties WIP
 
 export const registerClientSuccess = (id, formData) => ({
   type: actionTypes.REGISTER_CLIENT_SUCCESS,

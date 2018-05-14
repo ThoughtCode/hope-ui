c17efa39fa686a726dada2b1c68982aa4cb414ab
Rai Romero <rainieromadrid@gmail.com>
2018-05-14 11:51:58 -0500

    Fixing Contact
    Signed-off-by: Rai Romero <rainieromadrid@gmail.com>

--- src/containers/Home/Contact.js
+++ src/containers/Home/Contact.js
@@ -25,13 +25,13 @@ const styles = theme => ({
 
 function FullWidthGrid(props) {
   return (
-    <div className={cls.stylesContact}>
+    <div className={cls.Contact}>
       <Grid container spacing={24} justify="center">
         <Grid item xs={12} md={8} sm={12}>
           <Grid container>
             
             <Grid item xs={12} sm={12} md={4} lg={2}>
-              <Paper className={cls.styleSPaper} elevation={0}>
+              <Paper className={cls.Paper} elevation={0}>
                 <Typography variant="title">
                   <img src={LogoNocNoc} height="100px" alt="Logo"></img>
                 </Typography>
@@ -39,14 +39,14 @@ function FullWidthGrid(props) {
             </Grid>
             
             <Grid item xs={12} sm={6} md={8} lg={5}>
-              <Paper className={cls.stylePaper} elevation={0}>
+              <Paper className={cls.Paper} elevation={0}>
                 <form>
-                  <TextField id="email" label="E-mail" fullWidth margin = "normal" />
-                  <TextField id="name" label="Full Name" fullWidth margin = "normal" />
-                  <TextField id="multiline-static" label="Message" multiline rows="4" fullWidth margin = "normal" />
+                  <TextField id="email" label="E-mail" className={cls.textField} fullWidth margin="normal" />
+                  <TextField id="name" label="Full Name" className={cls.textField} fullWidth margin="normal" />
+                  <TextField id="multiline-static" label="Message" className={cls.textField} multiline rows="4" fullWidth margin="normal" />
                   <Grid container justify='flex-end'>
                     <Grid item>
-                      <Button className={cls.styleButton}>ENVIAR</Button>
+                      <Button className={cls.Button}>ENVIAR</Button>
                     </Grid>
                   </Grid>
                 </form>
@@ -54,26 +54,26 @@ function FullWidthGrid(props) {
             </Grid>
             
             <Grid item xs={12} sm={6} md={12} lg={5}>
-              <Paper className={cls.stylePaper} elevation={0}>
+              <Paper className={cls.Paper} elevation={0}>
                 <Grid container spacing={24}>
                   <Grid item xs={12}>
-                    <Paper className={cls.stylePaper} elevation={0}>
+                    <Paper className={cls.Paper} elevation={0}>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt numquam eius ducimus natus mollitia sequi nemo dicta.
                     </Paper>
                   </Grid>
                   <Grid item xs={12}>
-                    <Paper className={cls.stylePaper} elevation={0}>
+                    <Paper className={cls.Paper} elevation={0}>
                       <Typography gutterBottom style={{color:'#fff'}}>Teléfono:</Typography>
                       <Typography gutterBottom style={{color:'#fff'}}>E-mail:</Typography>
                     </Paper>
                   </Grid>
                   <Grid item xs={12} sm={3}>
-                    <Paper className={cls.stylePaper} elevation={0}>
+                    <Paper className={cls.Paper} elevation={0}>
                       <img src={Facebook} alt="AppLogo" className={cls.Styleicon} />
                     </Paper>
                   </Grid>
                   <Grid item xs={12} sm={3}>
-                    <Paper className={cls.stylePaper} elevation={0}>
+                    <Paper className={cls.Paper} elevation={0}>
                       <img src={Twitter} alt="AppLogo" className={cls.Styleicon} />
                     </Paper>
                   </Grid>

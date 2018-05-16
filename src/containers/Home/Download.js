753795ed280a1aea4314e685097d74c4d3d3ff23
José Castellanos <neuralboot19@gmail.com>
2018-05-16 17:49:47 -0500

    arrangement mockuHome

--- src/containers/Home/Download.js
+++ src/containers/Home/Download.js
@@ -7,6 +7,7 @@ import Typography from 'material-ui/Typography';
 //Component
 import GooglePlay from '../../assets/GooglePlay.png';
 import AppStore from '../../assets/AppStore.png';
+import Fondo from '../../assets/BackgrounDownload.png';
 import cls from './Download.css';
 
 const stylePaper = {
@@ -26,39 +27,45 @@ function Download(props) {
   return (
     <div className={`${cls.Download} ${cls.stylesDownload}`}>
       <Grid container justify="center">
-        <Grid item xs={12} md={8} sm={12}>
+        <Grid item xs={12} md={8} sm={12} lg={8}>
           <Grid container>
-            <Grid item xs={12} sm={8} lg={6}>
+            <Grid item xs={12} sm={12} md={8} lg={8}>
               <Paper className={classes.paper} elevation={0} style={stylePaper}>
                 <Grid item xs={12}>
                   <Paper className={classes.paper} elevation={0} style={stylePaper}>
-                    <Typography variant="headline" gutterBottom align="left" className={cls.Typogra}>Download our Download</Typography>
+                    <Typography variant="headline" gutterBottom align="left" className={cls.Typogra}>¡Descarga nuestra App y contrata nuestros servicios, es muy fácil!</Typography>
                   </Paper>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} sm={6} md={12}>
                   <Paper className={classes.paper} elevation={0} style={stylePaper}>
                     <Typography variant="subheading" gutterBottom align="left">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      <i className="material-icons">check</i>
+                      Contrata y agenda servicios
                     </Typography>
-                  </Paper>
-                </Grid>
-                <Grid item xs={12} >
-                  <Paper className={classes.paper} elevation={0} style={stylePaper}>
-                    <Grid container spacing={24}>
-                      <Grid item xs={12} sm={6} md={6} xl={6}>
-                        <Paper className={classes.paper} elevation={0} style={stylePaper}>
-                          <img src={GooglePlay}  alt="GoolglePlay" />
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={12} sm={6} md={6} xl={6}>
-                        <Paper className={classes.paper} elevation={0} style={stylePaper}>
-                          <img src={AppStore}  alt="App Store" />
-                        </Paper>
-                      </Grid>
-                    </Grid>
+                    <Typography variant="subheading" gutterBottom align="left">
+                      <i className="material-icons">check</i>
+                      Elige el perfil de tu agente
+                    </Typography>
+                    <Typography variant="subheading" gutterBottom align="left">
+                      <i className="material-icons">check</i>
+                      Paga con tus tarjetas preferidas
+                    </Typography>
+                    <Typography variant="subheading" gutterBottom align="left">
+                      <i className="material-icons">check</i>
+                      Recibe notificaciones
+                    </Typography>
+                    <Typography variant="subheading" gutterBottom align="left">
+                      <i className="material-icons">check</i>
+                      Recibe notificaciones
+                    </Typography>
+                    <img src={GooglePlay} className={cls.iconSocial} alt="GoolglePlay" />
+                    <img src={AppStore} className={cls.iconSocial} alt="App Store" />
                   </Paper>
                 </Grid>
               </Paper>
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} lg={4}>
+              <img src={Fondo}  alt="fondo" className={cls.img} />
             </Grid>
           </Grid>
         </Grid>
--- src/containers/Home/Testimonials.js
+++ src/containers/Home/Testimonials.js
@@ -5,9 +5,11 @@ import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Typography from 'material-ui/Typography';
+import Avatar from 'material-ui/Avatar';
 
 // Component
 import cls from './Testimonials.css'
+import Testimonials from '../../assets/Testimonials.jpg'
 
 const styles = theme => ({
   root: {
@@ -30,9 +32,10 @@ function FullWidthGrid(props) {
           <Grid container>
             <Grid item xs={12}>
               <Paper className={classes.paper} elevation={0}>
-                <Typography variant="headline" gutterBottom className={cls.Typogra}>Testimonials</Typography>
+                <Typography variant="headline" gutterBottom className={cls.Typogra}>Testimonios</Typography>
               </Paper>
             </Grid>
+           
             <Grid item xs={12} sm={12} md={4}>
               <Paper className={classes.paper} elevation={0}>
                 <Grid container>
@@ -50,26 +53,42 @@ function FullWidthGrid(props) {
             </Grid>
             <Grid item xs={12} sm={12} md={8}>
               <Paper className={classes.paper} elevation={0}>
-                <img align = 'right' className={cls.imagetestimonialsMain} src="http://sunrise-treecare.com/wp-content/uploads/2016/08/testimonialssmiley.png" alt="AppLogo" />
+                <Grid container>
+                  <Grid item>
+                    <img className={cls.imagetestimonialsMain} src={Testimonials} alt="AppLogo" />
+                  </Grid>
+                </Grid>
               </Paper>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
+            
+            <Grid container justify="center">
+              <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
+                <Paper className={classes.paper} elevation={0}>
+                  <Avatar alt="Adelle Charles"
+                  src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo"
+                  className={cls.imagetestimonials}/>
+                  <Typography variant="headline" gutterBottom className={cls.SubTytle}>"Testimonials 1"</Typography>
+                </Paper>
+              </Grid>
+            <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
               <Paper className={classes.paper} elevation={0}>
-                <img className={cls.imagetestimonials} src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo" />
+              <Avatar alt="Adelle Charles"
+                src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo"
+                className={cls.imagetestimonials}/>
                 <Typography variant="headline" gutterBottom className={cls.SubTytle}>"Testimonials 1"</Typography>
               </Paper>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
+            <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
               <Paper className={classes.paper} elevation={0}>
-                <img className={cls.imagetestimonials} src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo" />
+      <Avatar
+        alt="Adelle Charles"
+        src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo"
+        className={cls.imagetestimonials}
+      />
+
                 <Typography variant="headline" gutterBottom className={cls.SubTytle}>"Testimonials 1"</Typography>
               </Paper>
             </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <Paper className={classes.paper} elevation={0}>
-                <img className={cls.imagetestimonials} src="https://static.wixstatic.com/media/d5f2d1977eb9475b95ee2119f340a3eb.jpg/v1/fill/w_278,h_280,al_c,q_80,usm_0.66_1.00_0.01/d5f2d1977eb9475b95ee2119f340a3eb.webp" alt="AppLogo" />
-                <Typography variant="headline" gutterBottom className={cls.SubTytle}>"Testimonials 1"</Typography>
-              </Paper>
             </Grid>
           </Grid>
         </Grid>

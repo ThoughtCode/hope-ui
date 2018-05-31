652f7faf3aa4d450739e59f009f2006fcb4ba1f7
José Castellanos <neuralboot19@gmail.com>
2018-05-31 14:52:29 -0500

    Fix alt Services

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -45,7 +45,7 @@ function FullWidthGrid(props) {
               <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
                 <Paper className={classes.paper} elevation={0}>
                   <Typography variant="headline" gutterBottom className={cls.SubTytle}>Montaje de muebles</Typography>
-                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_assembly-094e26b64894275b61949dab65d23781.jpeg" alt="pinture" />
+                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_assembly-094e26b64894275b61949dab65d23781.jpeg" alt="Servicio Principal" />
                 </Paper>
               </Grid>
 
@@ -54,19 +54,19 @@ function FullWidthGrid(props) {
               <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
                 <Paper className={classes.paper} elevation={0}>
                   <Typography variant="headline" gutterBottom className={cls.SubTytle}>Montaje de TV</Typography>
-                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_tv_mounting-7404458fa9fea08dfb6b7623f89f2acd.jpeg" alt="pinture" />
+                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_tv_mounting-7404458fa9fea08dfb6b7623f89f2acd.jpeg" alt="Servicio Montaje TV" />
                 </Paper>
               </Grid>
               <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
                 <Paper className={classes.paper} elevation={0}>
                   <Typography variant="headline" gutterBottom className={cls.SubTytle}>Pintura interior</Typography>
-                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_painting-ffad5433ee803637b4a6759cd53eaff7.jpeg" alt="pinture" />
+                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_painting-ffad5433ee803637b4a6759cd53eaff7.jpeg" alt="Servicio Pintura Interio" />
                 </Paper>
               </Grid>
               <Grid item xs={12} sm={6} md={4} className={cls.TestimonialsContainer}>
                 <Paper className={classes.paper} elevation={0}>
                   <Typography variant="headline" gutterBottom className={cls.SubTytle}>Colgantes y estantes colgantes</Typography>
-                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_pictures-2b66751444ce3462a0303379a6754c3c.jpeg" alt="pinture" />
+                  <img className={cls.imageServices} src="//cache.hbfiles.com/assets/home/services_pictures-2b66751444ce3462a0303379a6754c3c.jpeg" alt="Servicio Colgantes" />
                 </Paper>
               </Grid>
             </Grid>

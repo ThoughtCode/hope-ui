c65c79e299af26b93dcce702e8b5c7ede25167d2
José Castellanos <neuralboot19@gmail.com>
2018-06-22 17:29:20 -0500

    Fix

--- src/components/Agent/Auth/Auth.js
+++ src/components/Agent/Auth/Auth.js
@@ -26,7 +26,7 @@ const styles = theme => ({
     border: '1px solid #ced4da',
     fontSize: 16,
     padding: '10px 12px',
-    width: 'calc(100% - 24px)',
+    width: 520,
     margin: 10,
     transition: theme.transitions.create(['border-color', 'box-shadow']),
     fontFamily: [

9d3deaba407d1ce9bb8e31c24c4e4c6504246778
Rai Romero <Rainieromadrid@gmail.com>
2018-07-07 15:01:43 -0500

    Changing service view
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- src/components/Client/HowWorks/HowWorks.js
+++ src/components/Client/HowWorks/HowWorks.js
@@ -8,6 +8,8 @@ import {
 // Css
 import cls from './HowWorks.css';
 
+import Check from '../../../assets/check.png';
+
 const howWorks = props => {
   return (
     <Grid container>
@@ -20,37 +22,37 @@ const howWorks = props => {
           </div>
           <ul>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
-                &nbsp;&nbsp;Elige el tipo de servicio que necesitas
+              &nbsp;&nbsp;Elige el tipo de servicio que necesitas
               </div>
             </li>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
                 &nbsp;&nbsp;Elige la frecuencia, dia y hora de nuestra visita
               </div>
             </li>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
                 &nbsp;&nbsp;Elige servicios adicionales
               </div>
             </li>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
                 &nbsp;&nbsp;Elige o registra tu direccion
               </div>
             </li>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
                 &nbsp;&nbsp;Paga tu servicio con una tarjeta de credito o debito
               </div>
             </li>
             <li className={cls.ListItem}>
-              <i className="fa fa-caret-right"></i>
+              <img src={Check} className={cls.Check} alt="check"/>
               <div className={cls.ListItemStep}>
                 &nbsp;&nbsp;Elige la fecha del servicio
               </div>

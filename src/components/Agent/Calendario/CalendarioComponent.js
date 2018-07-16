c4e0da453f06139fb213784b44c6e3d2e331e101
Rai Romero <Rainieromadrid@gmail.com>
2018-07-16 10:19:06 -0500

    Fixing minor
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- src/components/Agent/Calendario/CalendarioComponent.js
+++ src/components/Agent/Calendario/CalendarioComponent.js
@@ -13,91 +13,87 @@ import 'fullcalendar/dist/fullcalendar.print.min.css';
 import 'fullcalendar/dist/fullcalendar.js';
 
 class Calendario extends React.Component {
-    componentDidMount() {
-        const { calendar } = this.refs;
-        $(calendar).fullCalendar({
-            eventClick: function(eventObj) {
-                if (eventObj.id === 999) {
-                    alert(
-                    'Clicked Pepito' + eventObj.title
-                    );
-                }else if (eventObj.id === 888) {
-                    alert(
-                    'Clicked Pepito 888' + eventObj.title
-                    );
-                }
-                return false;
-            },
-            events: this.state.events,
-            eventLimit: 3,
-        });
-    }
-
-    constructor( props) {
-        super(props);
-        this.state = {
-            events:[
-                    {
-                        title: 'All Day Event Jose',
-                        start: '2018-06-05'
-                    },
-                    {
-                        title: 'All Day Event',
-                        start: '2018-06-05'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2018-06-05',
-                        end: '2018-06-10'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event clicker',
-                        start: '2018-06-05T16:00:00'
-                    },
-                    {
-                        id: 888,
-                        title: 'Repeating Event',
-                        start: '2018-06-05T16:00:00'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2018-06-05',
-                        end: '2017-05-13'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2018-06-12T10:30:00',
-                        end: '2018-06-12T12:30:00'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2018-06-13T07:00:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2018-06-11'
-                    }
-            ],		
+  state = {
+    events: [
+      {
+        title: 'All Day Event Jose',
+        start: '2018-06-05'
+      },
+      {
+        title: 'All Day Event',
+        start: '2018-06-05'
+      },
+      {
+        title: 'Long Event',
+        start: '2018-06-05',
+        end: '2018-06-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event clicker',
+        start: '2018-06-05T16:00:00'
+      },
+      {
+        id: 888,
+        title: 'Repeating Event',
+        start: '2018-06-05T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2018-06-05',
+        end: '2017-05-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2018-06-12T10:30:00',
+        end: '2018-06-12T12:30:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2018-06-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2018-06-11'
+      }
+    ],		
+  }
+  componentDidMount() {
+    const { calendar } = this.refs;
+    $(calendar).fullCalendar({
+      eventClick: (eventObj) => {
+        if (eventObj.id === 999) {
+          alert(
+          'Clicked Pepito' + eventObj.title
+          );
+        }else if (eventObj.id === 888) {
+          alert(
+          'Clicked Pepito 888' + eventObj.title
+          );
         }
-    }
+        return false;
+      },
+      events: this.state.events,
+      eventLimit: 3,
+    });
+  };
   render() {
-
     return (
-        <div>
-            <MenuBar />
-            <Grid container justify="center">
-                <Grid item xs={12}>
-                    <Paper elevation={0}>
-                        <div ref='calendar'></div>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
+      <div>
+        <MenuBar />
+        <Grid container justify="center">
+          <Grid item xs={12}>
+            <Paper elevation={0}>
+              <div ref='calendar'></div>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     );
-}
-}
+  };
+};
+
 document.getElementById('root')
 
 export default Calendario;
--- src/containers/Agent/Calendario/Calendario.js
+++ src/containers/Agent/Calendario/Calendario.js
@@ -11,33 +11,32 @@ import CalendarioComponent from '../../../components/Agent/Calendario/Calendario
 // Css
 import cls from './Calendario.css';
 
-class Calendario extends React.Component {
-    
+class Calendario extends React.Component {    
   render() {
     return (
-        <div>
-            <MenuBar />
-            <Grid container justify="center" className={cls.root}>
-                <Grid item xs={12} sm={10}>
-                    <Paper elevation={0}>
-                        <Grid container justify="center">
-                            <Grid item xs={12}>
-                                <Paper elevation={0}>
-                                    <Typography variant="title" gutterBottom className={cls.Typogra}>Calendario</Typography>
-                                </Paper>
-                            </Grid>
-                            <Grid item xs={12} sm={8}>
-                                <Paper elevation={0}>
-                                    <CalendarioComponent />
-                                </Paper>
-                            </Grid>
-                        </Grid>
-                    </Paper>
+      <div>
+        <MenuBar />
+        <Grid container justify="center" className={cls.root}>
+          <Grid item xs={12} sm={10}>
+            <Paper elevation={0}>
+              <Grid container justify="center">
+                <Grid item xs={12}>
+                  <Paper elevation={0}>
+                    <Typography variant="title" gutterBottom className={cls.Typogra}>Calendario</Typography>
+                  </Paper>
                 </Grid>
-            </Grid>
-        </div>
+                <Grid item xs={12} sm={8}>
+                  <Paper elevation={0}>
+                    <CalendarioComponent />
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     );
-}
+  };
 }
 
 export default Calendario;

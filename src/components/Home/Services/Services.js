20bd702bee6603ef539c2167cc72bf144a9bb085
José Castellanos <neuralboot19@gmail.com>
2018-11-20 11:03:04 -0500

    Fix Contratalo Button

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,7 +1,9 @@
 // Dependencias
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Modal from 'material-ui/Modal';
+import { withRouter } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import {
   Grid,
@@ -13,44 +15,89 @@ import {
 // Component
 import cls from './Services.css';
 import Service1 from '../../../assets/Service1.jpeg';
+import Registro from '../../../components/Client/Register/Register';
 
 const styles = theme => ({
   paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    backgroundColor: 'transparent',
+    position: 'absolute',
+    backgroundColor: theme.palette.background.paper,
   },
 });
 
-function FullWidthGrid(props) {
-  const { classes } = props;
-
-  return (
-    <div className={cls.root}>
-      <Grid container justify="center" className={cls.contenTitle}>
-        <Grid item xs={12}>
-          <Paper className={cls.Paper} elevation={0}>
-            <h1 variant="headline" gutterBottom className={cls.Typogra}>Servicios</h1>
-          </Paper>
+class FullWidthGrid extends Component {
+  state = {
+    openRegister: false,
+    openLogin: false,
+    openAgentLogin: false,
+  };
+  handleOpen = (modal) => {
+    console.log(modal, 'estoy aqui')
+    if (localStorage.getItem('signInAs') === 'customer') {
+      this.props.history.push('/cliente')
+    }
+    if (localStorage.getItem('signInAs') === 'agent') {
+      this.props.history.push('/agente')
+    }
+    if (modal === "register") {
+      this.setState({
+        openLogin: false,
+        openAgentLogin: false,
+        openRegister: true,
+      });
+    } else if (modal === "login") {
+      this.setState({
+        openLogin: true,
+        openAgentLogin: false,
+        openRegister: false,
+      });
+    } else if (modal === "loginAgent") {
+      this.setState({
+        openLogin: false,
+        openAgentLogin: true,
+        openRegister: false,
+      });
+    }
+  };
+  handleClose = () => {
+    this.setState({
+      openLogin: false,
+      openAgentLogin: false,
+      openRegister: false,
+    });
+  };
+  render(){
+    const { classes } = this.props;
+    return (
+      <div className={cls.root}>
+        <Grid container justify="center" className={cls.contenTitle}>
+          <Grid item xs={12}>
+            <Paper className={cls.Paper} elevation={0}>
+              <h1 variant="headline" gutterBottom className={cls.Typogra}>Servicios</h1>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container className={cls.Services} justify="center">
-        <Grid item xs={12} sm={6}>
-          <Paper className={`${cls.ContentServices} ${classes.paper}`} align="center" elevation={0}>
-            <img className={cls.ImageResponsive} src={Service1} alt="pinture" />
-            <div className={cls.footerServicios}>
-              <Typography variant="headline" gutterBottom className={cls.SubTytle} >Limpieza de hogar</Typography>
-              <Button className={cls.pageButtonActive} component={Link} to="/agente/registro">Contrátalo</Button>
-            </div>
-          </Paper>
+        <Grid container className={cls.Services} justify="center">
+          <Grid item xs={12} sm={6}>
+            <Paper className={cls.ContentServices} align="center" elevation={0}>
+              <img className={cls.ImageResponsive} src={Service1} alt="pinture" />
+              <div className={cls.footerServicios}>
+                <Typography variant="headline" gutterBottom className={cls.SubTytle} >Limpieza de hogar</Typography>
+                <Button className={cls.pageButtonActive} onClick={() => this.handleOpen("register")}>Contrátalo</Button>
+              </div>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  );
+        <Modal
+          open={this.state.openRegister}
+          onClose={this.handleClose}
+        >
+          <div className={`${cls.Modal} ${classes.paper}`}>
+            <Registro close={this.handleClose} />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
 
-FullWidthGrid.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(FullWidthGrid);
+export default withRouter(withStyles(styles)(FullWidthGrid));
